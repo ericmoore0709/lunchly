4f827e12f351e86c2c1701b28f6768b9474f0d30
Eric Moore <ericmoore0709@gmail.com>
2024-07-14 16:43:57 -0700

    Updated database

--- db.js
+++ db.js
@@ -1,8 +1,9 @@
 /** Database for lunchly */
 
 const pg = require("pg");
+require('dotenv').config();
 
-const db = new pg.Client("postgresql:///lunchly");
+const db = new pg.Client(process.env.POSTGRES_URI);
 
 db.connect();
 
